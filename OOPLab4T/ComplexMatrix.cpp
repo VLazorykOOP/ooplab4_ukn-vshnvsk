ac32c23752291e2974e22b635dbb1425d25209e5
vshnvsk <vyshnovska.yana@chnu.edu.ua>
2022-12-11 15:31:00 +0200

    done

--- OOPLab4T/ComplexMatrix.cpp
+++ OOPLab4T/ComplexMatrix.cpp
@@ -1,282 +1,252 @@
+#include <iostream>
 #include "ComplexMatrix.h"
+#include "ComplexVector.h"
+#include <math.h>
+
 const double EpsCalculations = 1.e-5;
-ComplexMatrix::ComplexMatrix(int ni, int mi, ComplexDouble b)
-{
-	if (ni <= 0) n = 2; else n = ni;
-	if (mi <= 0) m = 2; else m = mi;
-	 vec = new ComplexVector[n];
-	 for (int i = 0; i < n; i++) vec[i].Init(m, b);
-	// vec = new ComplexVector[n]{ ComplexVector(m,b) };
-	
 
-}
-
-ComplexMatrix::ComplexMatrix(const ComplexMatrix& s)
-{
-	n = s.n;
-	m = s.m;
-	int i;
-//	vec = new ComplexVector[n]{ ComplexVector(m) };
-	vec = new ComplexVector[n];    
-	for (i = 0; i < n; i++) vec[i].Init(m,0);
-	for (i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)  vec[i][j] = s.vec[i][j];
-
-}
-
-ComplexMatrix& ComplexMatrix::operator=(const ComplexMatrix& s)
-{
-	if (this != &s) {
-		int i;
-		if (n != s.n || m != s.m) {
-			n = s.n; m = s.m;
-			if (vec != nullptr) {
-					delete[] vec;
-			}
-				vec = new ComplexVector [n];
-				for (i = 0; i < n; i++) vec[i].Init(m,0);
+MatrixDouble::MatrixDouble(int n1) {
+	if (n < 0 || n > 1.e+100)
+		cout << " ERROR !!!\n";
+	else {
+		n = n1;
+		DoubleArray = new VectorDouble[n];
+		for (int i = 0; i < n; i++) {
+			DoubleArray[i] = VectorDouble(n);
 		}
-		for (i = 0; i < n; i++)
-			for (int j = 0; j < m; j++)  vec[i][j] = s.vec[i][j];
-		
 	}
-	return *this;
 }
 
-ComplexMatrix& ComplexMatrix::operator=(ComplexMatrix&& s) noexcept
-{
-	n = s.n; m = s.m;
-	vec = s.vec;
-	s.vec = nullptr;
-	s.n = 0; s.m = 0;
-	return *this;
-}
-
-void ComplexMatrix::Input()
-{
-}
-
-void ComplexMatrix::Output()
-{
-}
-
-bool ComplexMatrix::operator!() const
-{
-	return false;
-}
-
-bool ComplexMatrix::operator~() const
-{
-	return false;
-}
-
-ComplexVector& ComplexMatrix::operator[](int index)
-{
-	if (index >= 0 && index < n) return vec[index];
-	cout << " Error : operator[] - index index out of range \n";
-	return vec[0];
-}
-
-ComplexMatrix& ComplexMatrix::operator+=(const ComplexMatrix& s)
-{
-	if (s.n == n) for (int i = 0; i < n; i++) vec[i] += s.vec[i];
+MatrixDouble::MatrixDouble(int n1, int size1) {
+	if (n < 0 || n > 1.e+100 || size1 < 0 || size1 > 1.e+100)
+		cout << " ERROR !!!\n";
 	else {
-		cout << "Error: matrices of different dimensions \n";
-		cout << "The += operation was not performed. \n";
+		n = n1;
+		size = size1;
+		DoubleArray = new VectorDouble[n];
+		for (int i = 0; i < n; i++) {
+			DoubleArray[i] = VectorDouble(size1);
+		}
 	}
-	return *this;
 }
 
-ComplexMatrix& ComplexMatrix::operator+=(const ComplexDouble& b)
-{
-	for (int i = 0; i < n; i++) vec[i] += b;
-	return *this;
-}
-
-ComplexMatrix& ComplexMatrix::operator+=(const double& b)
-{
-	for (int i = 0; i < n; i++) vec[i] += b;
-	return *this;
-}
-
-ComplexMatrix ComplexMatrix::operator+(const ComplexMatrix& b)
-{
-	 ComplexMatrix mat(*this);
-	 mat += b;
-	 return mat;
-}
-
-ComplexMatrix ComplexMatrix::operator+(const ComplexDouble& b)
-{
-	ComplexMatrix mat(*this);
-	mat += b;
-	return mat;
-}
-
-ComplexMatrix ComplexMatrix::operator+(const double& b)
-{
-	ComplexMatrix mat(*this);
-	mat += b;
-	return mat;
-}
-
-ComplexMatrix& ComplexMatrix::operator-=(const ComplexMatrix& s)
-{
-	if (s.n == n) for (int i = 0; i < n; i++) vec[i] -= s.vec[i];
+MatrixDouble::MatrixDouble(int n1, int size1, double a){
+	if (n < 0 || n > 1.e+100 || size1 < 0 || size1 > 1.e+100)
+		cout << " ERROR !!!\n";
 	else {
-		cout << "Error: matrices of different dimensions \n";
-		cout << "The += operation was not performed. \n";
-	}
-	return *this;
-}
-
-ComplexMatrix& ComplexMatrix::operator-=(const ComplexDouble& b)
-{
-	for (int i = 0; i < n; i++) vec[i] -= b;
-	return *this;
-}
-
-ComplexMatrix& ComplexMatrix::operator-=(const double& b)
-{
-	for (int i = 0; i < n; i++) vec[i] -= b;
-	return *this;
-}
-
-ComplexMatrix ComplexMatrix::operator-(const ComplexMatrix& b)
-{
-	ComplexMatrix mat(*this);
-	mat -= b;
-	return mat;
-}
-
-ComplexMatrix ComplexMatrix::operator-(const ComplexDouble& b)
-{
-	ComplexMatrix mat(*this);
-	mat -= b;
-	return mat;
-}
-
-ComplexMatrix ComplexMatrix::operator-(const double& b)
-{
-	ComplexMatrix mat(*this);
-	mat -= b;
-	return mat;
-}
-
-
-ComplexMatrix& ComplexMatrix::operator*=(const ComplexDouble& b)
-{
-	for (int i = 0; i < n; i++) vec[i] *= b;
-	return *this;
-}
-
-ComplexMatrix& ComplexMatrix::operator*=(const double& b)
-{
-	for (int i = 0; i < n; i++) vec[i] *= b;
-	return *this;
-}
-
-ComplexMatrix ComplexMatrix::operator*(const ComplexMatrix& b)
-{
-	if (n == b.m && m == b.m) {
-		ComplexMatrix ret(m,n);
-		for(int i=0; i<n; i++)
-			for (int j = 0; j < m; j++)
-			{
-				ret[i][j] = 0;
-				for (int l = 0; l < m; l++) ret[i][j] += vec[i][l] * b.vec[l][j];
-			}
-		return ret;
-	}
-	else {
-		cout << "Error: matrices of different dimensions \n";
-		cout << "The *  operation was not performed. \n";
-		return *this;
+		n = n1;
+		size = size1;
+		DoubleArray = new VectorDouble[n];
+		for (int i = 0; i < n; i++) {
+			DoubleArray[i] = VectorDouble(size1, a);
+		}
 	}
 }
 
-ComplexVector ComplexMatrix::operator*(const ComplexVector& b)
-{
-	ComplexVector ret(n);
-	if (n == b.num) {
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < m; j++)
-			{				
-				 ret[i] += vec[i][j] * b.v[j];
-			}
+MatrixDouble::MatrixDouble(const MatrixDouble* s){
+	n = s->n;
+	DoubleArray = new VectorDouble[n];
+	for (int i = 0; i < n; i++) 
+		DoubleArray[i] = s->DoubleArray[i];
+	codeError = s->codeError;
+}
+
+double MatrixDouble::GetN() {
+	return n;
+}
+
+double MatrixDouble::GetSIZE() {
+	return size;
+}
+
+double MatrixDouble::GetCodeError() {
+	return codeError;
+}
+
+MatrixDouble& MatrixDouble::operator++(int) {
+	for (int i = 0; i < n; i++) DoubleArray[i]++;
+	return *this;
+}
+
+MatrixDouble& MatrixDouble::operator--(int) {
+	for (int i = 0; i < n; i++) DoubleArray[i]--;
+	return *this;
+}
+
+bool MatrixDouble::operator!() const {
+	for (int i = 0; i < n; i++)
+		if (!DoubleArray[i] == false)
+			return false;
+		else
+			return true;
+}
+
+MatrixDouble MatrixDouble::operator-() {
+	for (int i = 0; i < n; i++) -DoubleArray[i];
+	return *this;
+}
+
+MatrixDouble& MatrixDouble::operator=(MatrixDouble& s) {
+	for (int i = 0; i < n; i++) DoubleArray[i] = s.DoubleArray[i];
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator+(MatrixDouble b) {
+	for (int i = 0; i < n; i++)  DoubleArray[i] += b.DoubleArray[i];
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator+(int b) {
+	for (int i = 0; i < n; i++) DoubleArray[i] += b;
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator+(float b) {
+	for (int i = 0; i < n; i++) DoubleArray[i] += b;
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator-(MatrixDouble b) {
+	for (int i = 0; i < n; i++)  DoubleArray[i] -= b.DoubleArray[i];
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator-(int b) {
+	for (int i = 0; i < n; i++) DoubleArray[i] -= b;
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator-(float b) {
+	for (int i = 0; i < n; i++) DoubleArray[i] -= b;
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator*(float b) {
+	for (int i = 0; i < n; i++) DoubleArray[i] *= b;
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator/(int b) {
+	for (int i = 0; i < n; i++) DoubleArray[i] /= b;
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator+=(MatrixDouble b) {
+	for (int i = 0; i < n; i++)  DoubleArray[i] += b.DoubleArray[i];
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator+=(float b) {
+	for (int i = 0; i < n; i++) DoubleArray[i] += b;
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator-=(MatrixDouble b) {
+	for (int i = 0; i < n; i++)  DoubleArray[i] -= b.DoubleArray[i];
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator-=(float b) {
+	for (int i = 0; i < n; i++) DoubleArray[i] -= b;
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator*=(float b) {
+	for (int i = 0; i < n; i++) DoubleArray[i] *= b;
+	return *this;
+}
+
+MatrixDouble MatrixDouble::operator/=(int b) {
+	for (int i = 0; i < n; i++) DoubleArray[i] /= b;
+	return *this;
+}
+
+double& MatrixDouble::operator[](int index) {
+	if (index >= 0 || index < size) {
+		double DD = DoubleArray[0].GetVec(index);
+		return DD;
 	}
 	else {
-		cout << "Error: matrix and vector different dimensions \n";
-		cout << "The *  operation was not performed. \n";
+		cout << " Error : operator[] - index out of range \n";;
+		codeError = 1;
+		double Dd = DoubleArray[0].GetVec(0);
+		return Dd;
 	}
-	return ret;
 }
 
-ComplexMatrix ComplexMatrix::operator*(const ComplexDouble& b)
-{
-	ComplexMatrix mat(*this);
-	mat *= b;
-	return mat;
-}
+void MatrixDouble::OutputMat() {
 
-ComplexMatrix ComplexMatrix::operator*(const double& b)
-{
-	ComplexMatrix mat(*this);
-	mat *= b;
-	return mat;
-}
-
-
-ComplexMatrix& ComplexMatrix::operator/=(const ComplexDouble& b)
-{
-	if (fabs(b.real()) < EpsCalculations && fabs(b.imag()) < EpsCalculations)
-	{
-		std::cout << " Error Matrix opreation /= b \n";
+	for (int i = 0; i < n; i++) {
+		std::cout << "   ";
+		DoubleArray[i].Output(); 
+		std::cout << "\n";
 	}
-	else for (int i = 0; i < n; i++) vec[i] /= b;
-	return *this;
 }
 
-ComplexMatrix& ComplexMatrix::operator/=(const double& b)
+int main() 
 {
-	if (fabs(b) < EpsCalculations)
-	{
-		std::cout << " Error Matrix opreation /= b \n";
-	}
-	else for (int i = 0; i < n; i++) vec[i] /= b;
-	return *this;
-}
+	MatrixDouble A1(3), A2(3, 5), A3(3, 5, 15);
+	MatrixDouble B2(3, 2), B4(3, 5, 20), B5(3, 5, 0), B6(5, 3, 2),B3(A3);
+	std::cout << "Конструктор без параметрів: \n";
+	A1.OutputMat();
+	std::cout << "Конструктор з двома параметрами: \n";
+	A2.OutputMat();
+	std::cout << "Конструктор з трьома параметрами: \n";
+	A3.OutputMat();
+	std::cout << "Конструктор копій: \n";
+	B3.OutputMat();
+	std::cout << "n : ";
+	std::cout << A3.GetN();
+	std::cout << "\nsize : ";
+	std::cout << A3.GetSIZE();
+	std::cout << "\ncodeError : ";
+	std::cout << A3.GetCodeError();
+	std::cout << "\n";
+	std::cout << "Унарних префікс ++: \n";
+	A3++;
+	A3.OutputMat();
+	std::cout << "Унарних постфікс --: \n";
+	A3--;
+	A3.OutputMat();
+	std::cout << "\nУнарне логічне ! : \n";
+	std::cout << !A3;
+	std::cout << "\n";
+	std::cout << "\nУнарний арифметичний - : \n";
+	-A3;
+	A3.OutputMat();
+	std::cout << "\nПрисвоєння: копіює вектор : \n";
+	B4 = A3;
+	B4.OutputMat();
+	std::cout << "Додавання для двох матриць : \n";
+	(A3 + B4).OutputMat();
+	std::cout << "Додавання для матриці та скаляра типу int : \n";
+	(A3 + 10).OutputMat();
+	std::cout << "Додавання для матриці та скаляра типу float : \n";
+	(A3 + (float)2.5).OutputMat();
+	std::cout << "Віднімання для двох матриць : \n";
+	(A3 - B4).OutputMat();
+	std::cout << "Віднімання для матриці та скаляра типу int : \n";
+	(A3 - 10).OutputMat();
+	std::cout << "Віднімання для матриці та скаляра типу float : \n";
+	(A3 - (float)2.5).OutputMat();
+	std::cout << "Множення для матриці та скаляра типу float : \n";
+	(B4 * 50).OutputMat();
+	std::cout << "Ділення для матриці та скаляра типу int : \n";
+	(B4 / 5).OutputMat();
+	std::cout << "Присвоєння додавання для двох матриць : \n";
+	(B4 += A3).OutputMat();
+	std::cout << "Присвоєння додавання для матриці та скаляра типу float : \n";
+	(B4 += (float)2.3).OutputMat();
+	std::cout << "Присвоєння віднімання для двох матриць : \n";
+	(B4 -= A3).OutputMat();
+	std::cout << "Присвоєння віднімання для матриці та скаляра типу float : \n";
+	(B4 -= (float)2.3).OutputMat(); 
+	std::cout << "Присвоєння множення для матриці та скаляра типу float : \n";
+	(B4 *= (float)2.3).OutputMat(); 
+	std::cout << "Присвоєння ділення для матриці та скаляра типу float : \n";
+	(B4 /= 5).OutputMat(); 
+	std::cout << "\nOперація індексації: \n";
+	std::cout << A3[3] << "\n";
 
-
-
-ComplexMatrix ComplexMatrix::operator/(const ComplexDouble& b)
-{
-	ComplexMatrix mat(*this);
-	mat *= b;
-	return mat;
-}
-
-
-ComplexMatrix ComplexMatrix::operator/(const double& b)
-{
-	ComplexMatrix mat(*this);
-	mat *= b;
-	return mat;
-}
-
-void ComplexMatrix::RandComplexMatrix()
-{
-	for (int i = 0; i < n; i++) vec[i].RandComplexVector();
-}
-
-istream& operator>>(istream& is, ComplexMatrix& s)
-{
-	for (int i = 0; i < s.n; i++) is >> s.vec[i];
-	return is;
-}
-
-ostream& operator<<(ostream& os, ComplexMatrix& s)
-{
-	for (int i = 0; i < s.n; i++) os << s.vec[i];  // << endl;
-	return os;
+	return 0;
 }
